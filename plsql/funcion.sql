f11a04c9e9046a2fa42a94bcd5bbf2d347d7b47d
Ubuntu <ubuntu@ip-172-31-9-234.eu-west-1.compute.internal>
2022-03-09 16:55:44 +0000

    dnis

--- plsql/funcion.sql
+++ plsql/funcion.sql
@@ -1,9 +1,29 @@
 CREATE OR REPLACE FUNCTION letra_dni(numero_dni int4)
--- Oracle:  RETURN char IS
 RETURNS char AS $$
+DECLARE
+    letra_devolver char(1);
 BEGIN
     -- var tipo valor;
-    RETURN
+    SELECT LETRA INTO letra_devolver FROM LETRAS_DNI WHERE RESTO=MOD(numero_dni,23);
+    RETURN letra_devolver;
 END;
 $$ LANGUAGE plpgsql;
+-- Oracle:  RETURN char IS
 -- Oracle / show errors;
+
+CREATE OR REPLACE FUNCTION validar_letra()
+RETURNS trigger AS 
+$$
+BEGIN
+    IF ( letra_dni(NEW.numero_id) = NEW.letra_dni ) THEN
+    -- En caso de que no encajen el calculado y el suministrado... EXPLOSION!!!
+        RAISE EXCEPTION 'Letra de control del DNI incorrecta';
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+    
+-- Trigger: EVENTOS: INSERT or UPDATE
+CREATE TRIGGER validador_letra_dni BEFORE INSERT OR UPDATE Persona
+FOR EACH ROW EXECUTE PROCEDURE validar_letra();
