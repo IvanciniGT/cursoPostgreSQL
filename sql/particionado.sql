72b8e6f60c3e39fc80ff19c8f4fb4e5991f0d728
Ubuntu <ubuntu@ip-172-31-9-234.eu-west-1.compute.internal>
2022-03-10 16:01:35 +0000

    particionado

--- sql/particionado.sql
+++ sql/particionado.sql
@@ -1,18 +1,19 @@
 -- Personas
-CREATE TABLE personas (
-    id              SERIAL          NOT NULL PRIMARY KEY, 
+CREATE TABLE personas2 (
+    id              SERIAL          NOT NULL, 
     estado          SMALLINT        NOT NULL,
-    otros_datos     VARCHAR(100)    NOT NULL
+    otros_datos     VARCHAR(100)    NOT NULL,
+    PRIMARY KEY (id, estado)
 )   PARTITION BY LIST(estado);
 -- LIST: Una serie de valores predefinidos sobre los que particionar
 
-CREATE TABLE personas_alta PARTITION OF personas FOR VALUES IN (1);
-CREATE TABLE personas_baja PARTITION OF personas FOR VALUES IN (2);
-CREATE TABLE personas_otros PARTITION OF personas DEFAULT; -- el resto
+CREATE TABLE personas_alta PARTITION OF personas2 FOR VALUES IN (1);
+CREATE TABLE personas_baja PARTITION OF personas2 FOR VALUES IN (2);
+CREATE TABLE personas_otros PARTITION OF personas2 DEFAULT; -- el resto
 
-INSERT INTO personas (1,'Ivan');
-INSERT INTO personas (1,'Carlos');
-INSERT INTO personas (2,'Emiliano');
-INSERT INTO personas (2,'Gustavo');
-INSERT INTO personas (3,'Ramon');
-INSERT INTO personas (4,'Jose Luis');
+INSERT INTO personas2 (estado,otros_datos) VALUES (1,'Ivan');
+INSERT INTO personas2 (estado,otros_datos) VALUES (1,'Carlos');
+INSERT INTO personas2 (estado,otros_datos) VALUES (2,'Emiliano');
+INSERT INTO personas2 (estado,otros_datos) VALUES (2,'Gustavo');
+INSERT INTO personas2 (estado,otros_datos) VALUES (3,'Ramon');
+INSERT INTO personas2 (estado,otros_datos) VALUES (4,'Jose Luis');
